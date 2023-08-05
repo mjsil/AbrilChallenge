5c4fc2f6039b41178bbb802a98d2b21905482044
Maurilio Silveira <maurilio.agile@fleetcor.com.br>
2023-08-05 03:07:03 -0300

    feat: add theme

---
+++ src/theme/index.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    text: '#000000',
+    subtext: '#48484A',
+    white: '#FFFFFF',
+    background: '#D9D9D9',
+  },
+
+  fonts: {
+    title: 'Poppins-Bold',
+    subtitle: 'Poppins-SemiBold',
+    text: 'Poppins-Regular',
+  },
+};
