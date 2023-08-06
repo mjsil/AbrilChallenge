44611805193635a428d704ed023916384a24abda
Maurilio Silveira <maurilio.agile@fleetcor.com.br>
2023-08-06 20:37:57 -0300

    feat: change Theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,9 +1,16 @@
 export const theme = {
   colors: {
     text: '#000000',
-    subtext: '#48484A',
+
+    subtext_100: '#48484A',
+    subtext_500: '#86868A',
+
     white: '#FFFFFF',
-    background: '#D9D9D9',
+
+    background_100: '#D9D9D9',
+    background_500: '#E5E5E6',
+
+    border: '#EBEBEB',
   },
 
   fonts: {
