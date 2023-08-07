0a58c604d72d6841540e0f9cab5d4ec9fd476c39
Maurilio Silveira <maurilio.agile@fleetcor.com.br>
2023-08-07 19:04:48 -0300

    feat: change Container component

--- src/components/Layout/Container/styles.ts
+++ src/components/Layout/Container/styles.ts
@@ -4,5 +4,5 @@ import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 export const Content = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.white};
-  padding-top: ${getStatusBarHeight() + 40}px;
+  padding-top: ${getStatusBarHeight() + 20}px;
 `;
