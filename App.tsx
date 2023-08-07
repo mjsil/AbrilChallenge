32e60a0f1d9bf565b44faba53016a91257e4fd78
Maurilio Silveira <maurilio.agile@fleetcor.com.br>
2023-08-07 19:09:56 -0300

    feat: change configurations

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components/native';
 
+import { store } from '~/redux/store';
 import { theme } from '~/theme';
 import { Routes } from '~/routes';
 
 const App = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ThemeProvider theme={theme}>
-        <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
-        <Routes />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+          <Routes />
+        </ThemeProvider>
+      </Provider>
     </GestureHandlerRootView>
   );
 };
